346b19582acc5342259fe30ce42d63cd56af0dc0
EvgeniaBzh <bazhmaevaevgenia@gmail.com>
2025-03-10 00:31:48 +0200

    10.03

--- WebApplication1/Models/Manager.cs
+++ WebApplication1/Models/Manager.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WebApplication1.Models;
 
@@ -11,7 +15,9 @@ public partial class Manager
 
     public DateTime? Date { get; set; } = null!;
 
-    public string? Purposes { get; set; } = null!;
+    public string? Purpose { get; set; } = null!;
+
+    public string? Place { get; set; } = null!;
 
     public decimal? Cost { get; set; }
 
